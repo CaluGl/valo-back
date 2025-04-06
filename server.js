5fe43bd01f5ec2e10de74a32acd51487be10f200
luca <calu@calu.calu.com>
2025-04-06 15:06:40 +0200

    Fix: use ES module and node-fetch correctly

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+import express from "express";
+import fetch from "node-fetch";
+import cors from "cors";
+
+
 const express = require("express");
 const fetch = require("node-fetch");
 const cors = require("cors");
